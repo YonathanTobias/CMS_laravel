4192992cf4a8129eff991d40dba0989e12eb1db9
Yonathan Tobias Buttok <48828679+YonathanTobias@users.noreply.github.com>
2026-09-17 12:04:01 +0700

    bug fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Exceptions\PostTooLargeException;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,5 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (PostTooLargeException $e, Request $request) {
+            return back()
+                ->with('error', 'Ukuran total foto/berkas yang Anda unggah terlalu besar melebihi kapasitas server PHP. Silakan pilih foto dengan ukuran yang lebih kecil!')
+                ->withInput($request->except(['image', 'gallery', 'file', 'profile_image_file', 'poster_image']));
+        });
     })->create();
